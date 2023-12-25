12aa09946bc045b1aed0c1f5bb158fd72dc8bd83
Mandeep Taneja <mandeeptaneja21@outlook.com>
2023-12-26 00:16:04 +0530

    Add Viper to remove hardcoding

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,23 +2,22 @@ package db
 
 import (
 	"database/sql"
+	"github.com/PerfectoZ/GoLang-API/util"
 	_ "github.com/lib/pq"
 	"log"
 	"os"
 	"testing"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/SimpleBank?sslmode=disable"
-)
-
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Cannot load config ", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to Database", err)
 	}
--- main.go
+++ main.go
@@ -4,18 +4,18 @@ import (
 	"database/sql"
 	"github.com/PerfectoZ/GoLang-API/api"
 	db "github.com/PerfectoZ/GoLang-API/db/sqlc"
+	"github.com/PerfectoZ/GoLang-API/util"
 	_ "github.com/lib/pq"
 	"log"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/SimpleBank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to Database", err)
 	}
@@ -23,7 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("Cannot start server:", err)
